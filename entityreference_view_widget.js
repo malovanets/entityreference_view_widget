4d7047bb8eb0978d7d46db2a5f7c20d888721713
Martin Malovanets <malovanets@2712581.no-reply.drupal.org>
2013-12-12 15:03:58 +0000

    now supports any cardinality

--- entityreference_view_widget.js
+++ entityreference_view_widget.js
@@ -14,7 +14,10 @@ Drupal.behaviors.entityreferenceViewWidget = {
       return false;
     });
     $('#entityreference-view-widget-modal-submit .button').click(function(){
+      $('#modal-content .error').remove();
+
       var button = $(this);
+      var selected_amount = $(checkboxes + ':checked').length;
       var field_name = $('#entityreference-view-widget-field-name').val();
       var field_frontend_name = field_name.replace(/\_/g, '-');
       var widget_settings = JSON.parse($('#entityreference-view-widget-' + field_frontend_name + '-settings').val());
@@ -24,31 +27,37 @@ Drupal.behaviors.entityreferenceViewWidget = {
 
       $('#' + widget_settings.table_id + ' input[type=checkbox]:checked').each(function(){
         query_string += '&default_entity_ids[' + $(this).data('delta') + ']=' + $(this).val();
+        selected_amount++;
       });
 
-      $.ajax({
-        url: '/?q=entityreference_view_widget/ajax',
-        type: 'POST',
-        dataType: 'html',
-        data: query_string,
-        success: function(data) {
-          data && $('#' + widget_settings.table_id + ' tbody').html(data);
-          $('#' + widget_settings.table_id + ' tbody tr').each(function(){
-            var el = $(this);
-            if (!el.find('.tabledrag-handle').length) {
-              Drupal.tableDrag[widget_settings.table_id].makeDraggable(el.get(0));
-              el.find('td:last').addClass('tabledrag-hide');
-              if ($.cookie('Drupal.tableDrag.showWeight') == 1) {
-                el.find('.tabledrag-handle').hide();
+      if (widget_settings.cardinality > 0 && widget_settings.cardinality < selected_amount) {
+        $('#modal-content').prepend('<div class="messages error">Please select no more than ' + widget_settings.cardinality + ' values.</div>')
+      }
+      else {
+        $.ajax({
+          url: '/?q=entityreference_view_widget/ajax',
+          type: 'POST',
+          dataType: 'html',
+          data: query_string,
+          success: function(data) {
+            data && $('#' + widget_settings.table_id + ' tbody').html(data);
+            $('#' + widget_settings.table_id + ' tbody tr').each(function(){
+              var el = $(this);
+              if (!el.find('.tabledrag-handle').length) {
+                Drupal.tableDrag[widget_settings.table_id].makeDraggable(el.get(0));
+                el.find('td:last').addClass('tabledrag-hide');
+                if ($.cookie('Drupal.tableDrag.showWeight') == 1) {
+                  el.find('.tabledrag-handle').hide();
+                }
+                else {
+                  el.find('td:last').hide();
+                }
               }
-              else {
-                el.find('td:last').hide();
-              }
-            }
-          });
-          button.hasClass('modal-close') && Drupal.CTools.Modal.dismiss();
-        }
-      });
+            });
+            button.hasClass('modal-close') && Drupal.CTools.Modal.dismiss();
+          }
+        });
+      }
     });
   }
 }
